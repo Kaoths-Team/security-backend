2f487b992c6491b888a175b5905e1960c8de10dc
Kao Oat <tanawit139@gmail.com>
2020-11-03 00:03:28 +0700

    feat(security): add Cache-Control header

--- src/middlewares/cache-control.middleware.ts
+++ src/middlewares/cache-control.middleware.ts
@@ -2,7 +2,7 @@ import { Request, Response } from 'express';
 
 export function NoCache() {
   return function (req: Request, res: Response, next: Function) {
-    res.setHeader('Cache-Control', 'no-store');
+    res.setHeader('Cache-Control', 'no-cache');
     next();
   }
 }
